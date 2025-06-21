b40fb07025263c0420398c8e3ea0e944942ab6d3
raflialamsyah666 <raflialamsyah@student.uir.ac.id>
2025-06-21 11:51:09 +0700

    Toko BoltzLab update

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -2,13 +2,13 @@
   <div id="carouselExample" class="carousel slide my-4" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="C:\Users\User\Documents\New folder\my-toko-online-vue\public\carousel1.jpg" class="d-block w-50 mx-auto" alt="carousel 1" />
+        <img class="d-block w-50 mx-auto" alt="carousel 1" />
       </div>
       <div class="carousel-item">
-        <img src="C:\Users\User\Documents\New folder\my-toko-online-vue\public\carousel2.jpg" class="d-block w-50 mx-auto" alt="carousel 2" />
+        <img  class="d-block w-50 mx-auto" alt="carousel 2" />
       </div>
       <div class="carousel-item">
-        <img src="C:\Users\User\Documents\New folder\my-toko-online-vue\public\carousel3.jpg" class="d-block w-50 mx-auto" alt="carousel 3" />
+        <img  class="d-block w-50 mx-auto" alt="carousel 3" />
       </div>
     </div>
     <button
